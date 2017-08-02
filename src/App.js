6171a8ee0660ece997723d4cdc48531a56a9d490
john <john@fryolator.com>
2017-08-02 16:31:45 -0700

    Added signup/login links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import {
+  withRouter,
+  Link
+} from 'react-router-dom';
+import {
+  Nav,
+  Navbar
+} from 'react-bootstrap';
 import Routes from './Routes';
+import RouteNavItem from './components/RouteNavItem';
 import './App.css';
 
 class App extends Component {
+  
+  handleNavLink = (event) => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  }
+  
   render() {
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">TypeUp</Link>
+              <Link to="/">Scratch</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
+              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
-      <Routes />
+        <Routes />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
