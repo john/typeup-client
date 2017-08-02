66e4a63d942386869012be4c8b8327bf33383cc0
john <john@fryolator.com>
2017-08-02 16:16:56 -0700

    Add App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">TypeUp</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>TypeUp</h1>
+          <p>Don't Get Up. Type Up!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
