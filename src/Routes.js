50332e483d450db0e966ee41412ac47e462df385
john <john@fryolator.com>
2017-08-02 16:35:16 -0700

    Added 404 handling

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import NotFound from './containers/NotFound';
 
 import Home from './containers/Home';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+  
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={NotFound} />
   </Switch>
 );
